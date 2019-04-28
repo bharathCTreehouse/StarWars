f3eeb05d2c444b82629b8e4afe7dd177435a96fb
bharathCTreehouse <bharathc6787@gmail.com>
2019-04-29 00:01:47 +0530

    Initial set of files.
    Initial set of files.

---
+++ StarWars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWars
+//
+//  Created by Bharath Chandrashekar on 28/04/19.
+//  Copyright © 2019 Bharath Chandrashekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
